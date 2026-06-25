7e49897b96581bbcebf545517fcc79c74041ac24
Lucas Porto de A. Cavalcante <lucasportoac@gmail.com>
2026-06-25 09:25:51 -0300

    feat: cria estrutura inicial do projeto Spring Boot

---
+++ src/main/java/br/com/dbc/vetpulse/VetpulseBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.dbc.vetpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetpulseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetpulseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dbc/vetpulse/VetpulseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dbc.vetpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetpulseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
